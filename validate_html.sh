826d04e8549114494c35b8d51ce5fcc647172fc3
Lisa Brodner <ellduin@me.com>
2024-04-25 08:36:31 +0200

    added option to show all errors/warnings

--- validate_html.sh
+++ validate_html.sh
@@ -5,6 +5,7 @@
 
 DIRECTORY_PATH="out"
 REGEXP=".*(Trailing.slash|Consider.adding.a..lang.|Consider.using.the..h1..element.as.a.top.level.heading.only).*"
+#REGEXP="" # no filter, all errors/warnings
 
 # show info/warnings and errors
 PARAMS='--format gnu --asciiquotes '$DIRECTORY_PATH
