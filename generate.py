93ac098fb39ae3099289fd26f15ef6d1d5e94e5a
Lisa Brodner <ellduin@me.com>
2024-09-01 20:58:10 +0200

    adding ls-json schema.com Article tag to the head tags for the articles

--- generate.py
+++ generate.py
@@ -975,7 +975,7 @@ def html_generate_all_articles_by_category(db):
         "So machen's andere|",
         "Software-Test|",
         "Software|",
-        "Rubriken|" #! toc_category
+        "Rubriken|" # toc_category
     ]
 
     html_parts = []
@@ -1087,7 +1087,7 @@ def html_generate_all_article_previews(db):
 
 ### Write full HTML files
 
-def write_full_html_file(db, path, title, preview_img, body_html, body_class, comments=False):
+def write_full_html_file(db, path, title, preview_img, body_html, body_class, comments=False, additional_head_tags=''):
     latest_issue_path = db.issues[db.latest_issue_key()].issue_dir_name
     impressum_path = os.path.join(latest_issue_path, f"{FILENAME_IMPRINT}.html")
 
@@ -1148,6 +1148,9 @@ def write_full_html_file(db, path, title, preview_img, body_html, body_class, co
     </script>
     <script src="/{BASE_DIR}lunr.js"></script>
     <script src="/{BASE_DIR}search.js"></script>
+
+    {additional_head_tags}
+
     {isso_html1}
 </head>
 
@@ -1484,7 +1487,30 @@ def copy_and_modify_html(article, html_dest_path, pdf_path, prev_page_link, next
     preview_img = next((url for url in (article.img_urls if article.img_urls else [])), None)
 
 
-    write_full_html_file(db, html_dest_path, title, preview_img, body_html, 'one_article', True)
+    # add ls-json schema.com information
+
+    def create_ls_json(title, image_url, date_published, date_modified, authors):
+        #! TODO: back date the date
+        if not image_url:
+            image_url = "logo.png"
+
+        ls_json = f'''
+    {{
+      "@context": "https://schema.org",
+      "@type": "Article",
+      "headline": "{title}",
+      "image": [ "{image_url}" ],
+      "datePublished": "{date_published}",
+      "dateModified": "{date_published}"
+    }}
+    '''
+        return ls_json
+
+    iso_date_published = article.article_pubdate().strftime("%a, %d %b %Y %H:%M:%S %z")[:-5] + "GMT"
+    ls_json = create_ls_json(article.title, preview_img, iso_date_published, iso_date_published, "")
+    head_html = f'<script type="application/ld+json">{ls_json}</script>'
+
+    write_full_html_file(db, html_dest_path, title, preview_img, body_html, 'one_article', True, head_html)
 
 def copy_articles_and_assets(db, in_directory, out_directory):
     if not os.path.exists(out_directory):
